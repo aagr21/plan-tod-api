625654008605fbb5b0b08121fa4a583613ffd107
Arnol Guevara <arnolpacman@gmail.com>
2024-09-27 10:37:55 -0400

    updated

--- src/root/models/entities/accessed-file-log.entity.ts
+++ src/root/models/entities/accessed-file-log.entity.ts
@@ -9,22 +9,6 @@ import {
 import { Institution } from './institution.entity';
 import { Directory } from './directory.entity';
 
-import { ValueTransformer } from 'typeorm';
-
-export class DateStringTransformer implements ValueTransformer {
-  to(value: Date): string {
-    if (!value) return null;
-    // Convertir a string en el formato deseado
-    return value.toISOString().slice(0, 19).replace('T', ' '); // Formato: "YYYY-MM-DD HH:mm:ss"
-  }
-
-  from(value: string): Date {
-    if (!value) return null;
-    // Convertir de string a Date
-    return new Date(value);
-  }
-}
-
 @Entity({
   name: 'accessed_files_logs',
 })
@@ -51,7 +35,6 @@ export class AccessedFileLog {
     name: 'accessed_at',
     type: 'timestamp with time zone',
     default: () => 'CURRENT_TIMESTAMP(6)',
-    transformer: new DateStringTransformer(), // Aplica el transformer aquí
   })
   accessedAt?: Date;
 
